60fffd5330b76d142391c03670c8b037840aad16
Isaac <isaacbruno1996@gmail.com>
2023-11-25 15:38:30 -0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:new, :create]  # Apenas as rotas necessárias para o login
+  resources :sessions, only: [:new, :create] 
 
   resources :users do
-    resources :time_logs, except: [:new]  # Removendo a rota 'new'
+    resources :time_logs, except: [:new]
     get 'new_registro_de_horas', to: 'time_logs#new_registro_de_horas', on: :member
     resources :project_memberships
   end
-  
 
   resources :projects do
     resources :task_types do
@@ -17,5 +16,5 @@ Rails.application.routes.draw do
 
   post '/login', to: 'sessions#create', as: :login
   delete '/logout', to: 'sessions#destroy', as: :logout
-  root to: "sessions#new"  # Alterado para apontar para a ação index
+  root to: "sessions#new"
 end
